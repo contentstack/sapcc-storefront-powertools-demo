3230e1c178925abd2892ab5f5d742f607719a509
Rushikesh Argade <rushikesh.argade@contentstack.com>
2026-08-20 12:55:13 +0530

    Add Spartacus 221121.15.1 storefront + Contentstack connector wiring

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,19 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  provideBrowserGlobalErrorListeners,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
+import { provideHttpClient, withFetch, withInterceptorsFromDi } from '@angular/common/http';
+import { AppModule } from './app.module';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    provideHttpClient(withFetch(), withInterceptorsFromDi()),
+    importProvidersFrom(AppModule),
+  ],
 };
